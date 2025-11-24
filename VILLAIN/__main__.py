05de9c976912651651d603e30fc1d7f6db85e500
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-11-24 11:23:09 +0530

    ─ ❖ | ʀɪsʜυ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖ ─

---
+++ VILLAIN/__main__.py
@@ -0,0 +1,59 @@
+
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from VILLAIN import LOGGER, app, userbot
+from VILLAIN.core.call import PRO
+from VILLAIN.misc import sudo
+from VILLAIN.plugins import ALL_MODULES
+from VILLAIN.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+from VILLAIN.plugins.tools.clone import restart_bots
+
+
+async def init():
+    if not config.STRING1:
+        LOGGER(__name__).error("String Session not filled, please provide a valid session.")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("VILLAIN.plugins" + all_module)
+    LOGGER("VILLAIN.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    await userbot.start()
+    await PRO.start()
+    try:
+        await PRO.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("VILLAIN").error(
+            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
+        )
+        exit()
+    except:
+        pass
+    await PRO.decorators()
+    await restart_bots()
+    LOGGER("VILLAIN").info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗞𝗔𝗡𝗛𝗔 𝐁𝐎𝐓𝐒☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("VILLAIN").info("𝗦𝗧𝗢𝗣 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
