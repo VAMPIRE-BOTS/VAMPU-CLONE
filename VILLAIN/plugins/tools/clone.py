70677bba162d8b94ed0b64fa2db77c961c891b55
UFC_UPDATES <ufcupdates313@gmail.com>
2025-12-13 20:25:16 +0530

    Update clone.py

--- VILLAIN/plugins/tools/clone.py
+++ VILLAIN/plugins/tools/clone.py
@@ -102,8 +102,8 @@ async def clone_txt(client, message, _):
                 "name": bot.first_name,
                 "token": bot_token,
                 "username": bot.username,
-                "channel": "aboutkanha",
-                "support": "Kanhasworld",
+                "channel": "ll_P_U_L_lI",
+                "support": "l_HEARTBEAT_l",
                 "premium" : True,
                 "Date" : False,
             }
