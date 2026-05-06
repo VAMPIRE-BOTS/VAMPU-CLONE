3e1727d5c9e788f0e11a6f5ef7d9b293cdf4ba84
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-06 20:07:25 +0530

    Update userbot.py

--- VILLAIN/core/userbot.py
+++ VILLAIN/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("oldskoolgc")
-                await self.one.join_chat("iamvillain77")
+                await self.one.join_chat("VAMPIREUPDATES")
+                await self.one.join_chat("VAMPIREUPDATES")
             except:
                 pass
             assistants.append(1)
